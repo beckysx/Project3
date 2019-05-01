e5c23bffc77f23927de82481286244757f657566
bexkysx <xu.song@centre.edu>
2019-04-30 22:40:57 -0400

    ad

--- js.js
+++ js.js
@@ -1,10 +1,11 @@
-var dataset=[
-{'year':"2014","ticket":378415107,"children":[{},{}]},
-{'year':"2015","ticket":513823537,"children":[{},{}]},
-{'year':"2016","ticket":455242712,"children":[{},{}]},
-{'year':"2017","ticket":442991687,"children":[{},{}]},
-{'year':"2018","ticket":557618269,"children":[{},{}]}
-]
+var dataset={"name":"Five years","ticket":2348091312,"children":[
+  {'year':"2014","ticket":378415107,"children":[{},{}]},
+  {'year':"2015","ticket":513823537,"children":[{},{}]},
+  {'year':"2016","ticket":455242712,"children":[{},{}]},
+  {'year':"2017","ticket":442991687,"children":[{},{}]},
+  {'year':"2018","ticket":557618269,"children":[{},{}]}
+]}
+
 var o14=d3.csv("2014Oscar.csv")
 var t14=d3.csv("2014Top.csv")
 var o15=d3.csv("2015Oscar.csv")
@@ -19,34 +20,87 @@ var t18=d3.csv("2018Top.csv")
 Promise.all([o14,t14,o15,t15,o16,t16,o17,t17,o18,t18]).then(function(d){
   for(i=0;i<d.length;i++){
   if (i%2!=0){
-    dataset[(i-1)/2].children[0].name="top"
-    dataset[(i-1)/2].children[0].children=[]
+    dataset.children[(i-1)/2].children[0].name="top"
+    dataset.children[(i-1)/2].children[0].children=[]
     var tsum=0
     d[i].forEach(function(a){
       tsum=tsum+parseInt(a.TicketsSold)
         var ob={"name":a.Name,"ticket":parseInt(a.TicketsSold),"rate":a.Rate,"genre":a.Genre}
-        dataset[(i-1)/2].children[0].children.push(ob)})
-    dataset[(i-1)/2].children[0].ticket=tsum
+        dataset.children[(i-1)/2].children[0].children.push(ob)})
+    dataset.children[(i-1)/2].children[0].ticket=tsum
     }
   else{
-    dataset[i/2].children[1].name="oscar"
-    dataset[i/2].children[1].children=[]
+    dataset.children[i/2].children[1].name="oscar"
+    dataset.children[i/2].children[1].children=[]
     var tsum=0
     d[i].forEach(function(a){
       tsum=tsum+parseInt(a.TicketsSold)
         var ob={"name":a.Name,"ticket":parseInt(a.TicketsSold),"rate":a.Rate,"genre":a.Genre}
-        dataset[i/2].children[1].children.push(ob)})
-    dataset[i/2].children[1].ticket=tsum
+        dataset.children[i/2].children[1].children.push(ob)})
+    dataset.children[i/2].children[1].ticket=tsum
 }
   }
-console.log(dataset)
+
+  var screen={width:1700,height:1300};
+  var margin = {top: 150, right: 100, bottom: 150, left: 100};
+  var w = screen.width - margin.left - margin.right;
+  var h = screen.height - margin.top - margin.bottom;
+
+  var svg=d3.select("body").append("svg")
+  .attr('width', screen.width)
+  .attr('height', screen.height)
 
 var pack=d3.pack()
-.size([1000,700])
+.size([w,h])
+.padding(4)
 
 var root=d3.hierarchy(dataset)
-.sum(function(d){
-  return d.value
-})
- var nodes=pack.nodes
+            .sum(function(d){
+              return d.ticket})
+            .sort(function(a,b){
+              return b.ticket-a.ticket})
+
+let nodes=pack(root).descendants()
+console.log(nodes)
+
+// color scale
+var layer2=d3.scaleOrdinal()
+.domain([2014,2015,2016,2017,2018])
+.range(["#A7BCC6","#CFBCCC","#B9B4B4","#C4AF89","#F3CC89"])
+
+svg.append("g").selectAll("circle").data(nodes)
+.enter()
+    .append('circle')
+    .attr('cx', function(d){return d.x})
+    .attr('cy', function(d){return d.y})
+    .attr('r',function(d){return d.r} )
+    .style('fill', function(d){
+      if (d.height==3){
+        return "white"
+      }
+      else if (d.height==2){
+        return "#A7BCC6"
+      }
+      else if (d.height==1){
+        if (d.data.name=="top"){return "#F9B4A4"}
+        else {
+          return "#D5909C"
+        }
+      }
+      else if (d.height==0){
+        return "white"
+      }
+    })
+    .attr('fill-opacity', 0.5)
+    .on('mouseover', function(){
+      var a=d3.select(this)
+      a.style('fill-opacity', 1)
+      .attr('stroke', 'black')
+      .attr('stroke-width', 2)
+    })
+    .on('mouseout', function(){
+      var a=d3.select(this)
+      a.style('fill-opacity', 0.5)
+      .attr('stroke', 'none')
+    })
 })
